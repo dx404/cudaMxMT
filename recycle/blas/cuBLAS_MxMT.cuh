be3f18d4b686486c5c503a7cb94983ead46cfdf2
wisdompilot <duozhao@cs.unc.edu>
2013-12-06 22:41:14 -0600

    final

--- recycle/blas/cuBLAS_MxMT.cuh
+++
@@ -1,3 +0,0 @@
-#include "cudaGFlopTimer.cuh"
-
-float cuBLAS_MxMT(float *h_m, float *h_r, int d);
--- sample.cu
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "src/cudaGFlopTimer.cuh"
-using namespace std;
-
-int main(){
-  cudaGFlopTimer *timer = new cudaGFlopTimer();
-  timer->start();
-  return 0;
-}
